5d740c9bcc05153222508afda16a3f3024848d6d
Mintorang <ehinderomdo1@gmail.com>
2023-11-25 08:01:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+try:
 
-url = "https://www.ebay.co.uk/sch/i.html?_nkw=faulty+ps4+consoles&_sop=12"
+    # This script uses Selenium to web scrape Ebay for a user-specified item
 
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+    # Imports used for the file
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    # Sets the browser that will be used, which in this case is Chrome
+    driver = webdriver.Chrome()
 
-#Get the Items
-confused_prices = soup.find_all("span", class_="s-item__price")
-num = 1
-re_arranged_prices = []
-for i in range(60):
+    # Declares the lists and dictionary that needs to be used for the script to function
+    ebay_links = []
+    item_content = {}
+    ebay_prices  = []
+
+    # Get User_input for the item the user is searching for
+    user_input = input("What do you want to search for?  ")
+    # Open ebay.co.uk and enter user_input in the search box
+    driver.get("https://www.ebay.co.uk")
+    ebay_search_box = driver.find_element(By.NAME, "_nkw")
+    ebay_search_box.click()
+    ebay_search_box.send_keys(f"{user_input}")
+    ebay_search_box.send_keys(Keys.ENTER)
+
+
+    # Stores all the links on the web page, which are still in HTML format, in a variable called links (this variable is a list)
+    links = driver.find_elements(By.CLASS_NAME, "s-item__link")
+
+
+    # Loops the links var and takes out the href from the link tag in html. Append the links to another list called: ebay_prices
+    for link in links:
+        new_link = link.get_attribute("href")
+        ebay_links.append(new_link)
+
+
+    # Loops thorough every link in ebay_links, to get the price and description. Updates item_content dictionary with all the necessary details. 
+    for link in ebay_links:
+        driver.get(link)
+        price = driver.find_element(By.CLASS_NAME,"x-price-primary")
+        description = driver.find_element(By.CLASS_NAME, "x-item-title__mainTitle")
+        ebay_prices.append(float(price.text.replace("£", "").replace("each", "")))
+        ebay_prices.sort()
+        item_content.update({f"{price.text}": [f"{link}", f"{description.text}"]})
+
+
         
-    x = str(confused_prices[num]).split("-->", 4)
-    num += 1
-    y = x[2].split("<!-", -3)
-    re_arranged_prices.append(y[0])
-print(len(re_arranged_prices))
-num = 0
-for i in range(60):
-    re_arranged_prices[num] = float(re_arranged_prices[num].replace("£", ""))
-    num +=1
-re_arranged_prices.sort()
-print(re_arranged_prices)
 
+    # Quits the chromedriver and concludes the program
+    driver.quit()
 
-#Get the links
-confused_links = soup.find_all("a", class_="s-item__link")
-print(confused_links[0])
+except error:
+    print("There was an error with the program. Please search for a different item")
